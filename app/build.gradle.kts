f46a2730b0ca12c1f7219b8d2d73c47a9cb4418f
deac-hary <crupa6648@gmail.com>
2026-08-03 22:46:53 +0530

    Fix release keystore path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 
 signingConfigs {
     create("release") {
-        storeFile = file(keystoreProperties["storeFile"] as String)
+        storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
         storePassword = keystoreProperties["storePassword"] as String
         keyAlias = keystoreProperties["keyAlias"] as String
         keyPassword = keystoreProperties["keyPassword"] as String

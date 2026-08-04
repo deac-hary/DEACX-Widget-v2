3494560fdad1bb9ce6a98a8caac338899399960d
deac-hary <crupa6648@gmail.com>
2026-08-04 18:50:31 +0530

    Add weather debug logs

--- app/src/main/java/com/deacx/widget/weather/WeatherUpdater.kt
+++ app/src/main/java/com/deacx/widget/weather/WeatherUpdater.kt
@@ -1,15 +1,27 @@
 package com.deacx.widget.weather
 
 import android.content.Context
+import android.util.Log
 import com.deacx.widget.data.local.deacxDataStore
 import com.deacx.widget.data.local.setWeatherEmoji
 import com.deacx.widget.location.LocationHelper
 
 object WeatherUpdater {
 
+    private const val TAG = "WeatherUpdater"
+
     suspend fun update(context: Context) {
 
-        val location = LocationHelper.getLastLocation(context) ?: return
+        Log.d(TAG, "Weather update started")
+
+        val location = LocationHelper.getLastLocation(context)
+
+        if (location == null) {
+            Log.e(TAG, "Location is NULL")
+            return
+        }
+
+        Log.d(TAG, "Location = ${location.first}, ${location.second}")
 
         val repository = WeatherRepository()
 
@@ -19,7 +31,15 @@ object WeatherUpdater {
                 longitude = location.second
             )
         }.onSuccess { weather ->
+
+            Log.d(TAG, "Weather emoji = ${weather.emoji}")
+
             context.deacxDataStore.setWeatherEmoji(weather.emoji)
+
+            Log.d(TAG, "Emoji saved successfully")
+        }.onFailure { e ->
+
+            Log.e(TAG, "Weather API failed", e)
         }
     }
 }

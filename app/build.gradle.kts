9ab3b6f645443467f5d2d90d282c7147ecfef0ee
deac-hary <crupa6648@gmail.com>
2026-08-04 16:27:41 +0530

    Add Retrofit dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,8 @@ dependencies {
     ksp(libs.hilt.compiler)
 
     testImplementation(libs.junit)
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }

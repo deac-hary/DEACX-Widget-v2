8ca90ece56ebf7e1a9110973a43b7fc0d74559b1
deac-hary <crupa6648@gmail.com>
2026-08-04 20:06:06 +0530

    Add coroutines Play Services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
 }
